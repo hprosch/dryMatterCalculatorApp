704d925d854e790f3c79bd045f045d721e1db362
hprosch <hprosch@stumail.jccc.edu>
2023-07-10 16:15:00 -0500

    Adding calculation code in python

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-#from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash
 
-#app = Flask(__name__)
-#app.secret_key = "gkudfgdfhgowrign"
+app = Flask(__name__)
+app.secret_key = "gkudfgdfhgowrign"
 
-#@app.route()
-#def index():
-#    flash()
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST", "GET"])
+def result():
+    protein = float(request.form['protein_input'])
+    fat = float(request.form['fat_input'])
+    fiber = float(request.form['fiber_input'])
+    moisture = float(request.form['moisture_input'])
+    other = 100 - protein - fat - fiber - moisture
+    
+    protein = ((protein * 100) / (100 - moisture))
+    fat = ((fat * 100) / (100 - moisture))
+    fiber = ((fiber * 100) / (100 - moisture))
+    other = ((other * 100) / (100 - moisture))
+
+    flash("Protein: " + str(round(protein, 2))+ "%")
+    flash("Fat: " + str(round(fat, 2)) + "%")
+    flash("Fiber: " + str(round(fiber, 2)) + "%")
+    flash("Other: " + str(round(other, 2)) + "%")
+    return render_template("index.html")
--- static/js/script.js
+++ static/js/script.js
@@ -1,82 +1,13 @@
 "use strict";
-//Select the calculate button:
-let calculate = document.getElementById('calculate');
+
+let calculate = document.getElementById('result');
 
 //Selecting table elements to alter later:
-let resultTable = document.getElementById('resultTable');
-resultTable.classList.add('hidden');
+let resultHeader = document.getElementById('resultHeader');
+resultHeader.classList.add('hidden');
 
-let dmProteinResult = document.getElementById('dmProtein');
-let dmFatResult = document.getElementById('dmFat');
-let dmFiberResult = document.getElementById('dmFiber');
-let dmOtherResult = document.getElementById('other');
-
-/*Grabbing Error Message Areas:*/
-let errorProtein = document.getElementById('spanProtein');
-let errorFat = document.getElementById('spanFat');
-let errorFiber = document.getElementById('spanFiber');
-let errorMoisture = document.getElementById('spanMoisture');
-
-let protein;
-let fat;
-let fiber;
-let moisture;
-let other;
-
-function clearErrorMessages() {
-	errorProtein.textContent = "";
-	errorFat.textContent = "";
-	errorFiber.textContent = "";
-	errorMoisture.textContent = "";
+function showResultHeader() {
+	resultHeader.classList.remove('hidden');
 }
 
-function validateInput() {
-
-	clearErrorMessages();
-	resultTable.classList.add('hidden');
-
-	let errorCount = 0;
-
-	protein = +document.getElementById('asProtein').value;
-	fat = +document.getElementById('asFat').value;
-	fiber = +document.getElementById('asFiber').value;
-	moisture = +document.getElementById('asMoisture').value;
-
-	if (protein == 0 || protein == null) {
-		errorProtein.textContent = "Please enter a value for protein";
-		errorCount++;
-	}
-	if (fat == 0 || fat == null) {
-		errorFat.textContent = "Please enter a value for fat";
-		errorCount++;
-	}
-	if (fiber == 0 || fiber == null) {
-		errorFiber.textContent = "Please enter a value for fiber";
-		errorCount++;
-	}
-	if (moisture == 0 || moisture == null) {
-		errorMoisture.textContent = "Please enter a value for moisture";
-		errorCount++;
-	}
-	if (errorCount == 0) {
-		calculateDryMatterPercentages();
-	}
-}
-
-
-function calculateDryMatterPercentages() {
-
-	clearErrorMessages();
-
-	other = 100 - protein - fat - fiber - moisture;
-
-	dmProteinResult.textContent = ((protein * 100) / (100 - moisture)).toFixed(2);
-	dmFatResult.textContent = ((fat * 100) / (100 - moisture)).toFixed(2);
-	dmFiberResult.textContent = ((fiber * 100) / (100 - moisture)).toFixed(2);
-	dmOtherResult.textContent = ((other * 100) / (100 - moisture)).toFixed(2);
-
-	resultTable.classList.remove('hidden');
-
-}
-
-calculate.addEventListener("click", validateInput);
+calculate.addEventListener("click", showResultHeader);
