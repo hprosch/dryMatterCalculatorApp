9c757c7a2a32e07fbd781965f2d421d328b73536
hprosch <hprosch@stumail.jccc.edu>
2023-07-17 16:59:53 -0500

    Added notes to JavaScript file.

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,8 @@
 let phospherousAsFed = document.getElementById('phospherousAsFed');
 let phospherous = document.getElementById('phospherous');
 
+/*When page loads, check if there was a value entered for 
+phospherous.  If not, then hide the phospherous row from the table:*/
 window.onload = function() {
 	if (phospherousAsFed.textContent === '%') {
 		phospherous.classList.add("hidden");
